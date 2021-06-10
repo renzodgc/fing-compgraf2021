ad078286b7097fae512c21dd073ed9eaba5555f3
Renzo Gambone <42361379+renzodgc@users.noreply.github.com>
2021-06-09 22:01:25 -0300

    Update obl2/obl2/renderer.cpp

--- obl2/obl2/renderer.cpp
+++ obl2/obl2/renderer.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 		cin.get();
 	}
 	else {
-		cout << "Error: El archivo config no pudo ser cargado" << endl;
+		cerr << "Error: El archivo config no pudo ser cargado" << endl;
 		cin.get();
 	}
 

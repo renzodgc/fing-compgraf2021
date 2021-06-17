e9675182ae2cb6d80f0da875c95d8e44623ea1bd
Germán Ouviña <germanouvina@gmail.com>
2021-06-17 13:52:04 -0300

    Base structure

--- obl2/obl2/file_helper.cpp
+++ obl2/obl2/file_helper.cpp
@@ -15,10 +15,5 @@ bool create_directory(string path) {
 
 bool save_image(Image* image, string file_name) {
 	FIBITMAP* bitmap = image->float_to_bitmap();
-	if (FreeImage_Save(FIF_PNG, bitmap, file_name.c_str(), 0)) {
-		cout << "Imagen exitosamente guardada" << endl;
-		return true;
-	}
-	cerr << "Error: Ha ocurrido un error al guardar la imagen" << endl;
-	return false;
+	return FreeImage_Save(FIF_PNG, bitmap, file_name.c_str(), 0);
 }
--- obl2/obl2/renderer.cpp
+++ obl2/obl2/renderer.cpp
@@ -2,32 +2,54 @@
 
 using namespace std;
 
+void pre_render() {
+	FreeImage_Initialise();
+}
+
+void post_render() {
+	FreeImage_DeInitialise();
+}
+
 int main(int argc, char* argv[]) {
 	
-	FreeImage_Initialise();
+	// 0. Pre-render: Initialise freeimage
+	pre_render();
 
+	// 1. Load scene from config file
 	Scene& scene_manager = Scene::get_instance();
-	if (scene_manager.is_loaded()) {
-		cout << "OK: El archivo config ha sido cargado correctamente" << endl << endl;
-
-		Story& story_manager = Story::get_instance();
-		if (story_manager.is_created()) {
-			cout << "OK: El directorio de historial ha sido creado correctamente" << endl << endl;
-			cout << "Iniciando Ray Tracing:" << endl;
-			story_manager.run_ray_tracing();
-		}
-		else {
-			cerr << "Error: El directorio de historial no pudo ser creado" << endl << endl;
-		}
-
-		cin.get();
-	}
-	else {
+	if (!scene_manager.is_loaded()) {
 		cerr << "Error: El archivo config no pudo ser cargado" << endl << endl;
+		post_render();
 		cin.get();
+		return -1;
 	}
+	cout << "1. La escena ha sido cargado correctamente" << endl;
 
-	FreeImage_DeInitialise();
+	// 2. Create story directory for saving intermediate and final images
+	Story& story_manager = Story::get_instance();
+	if (!story_manager.is_created()) {
+		cerr << "Error: El directorio de historial no pudo ser creado" << endl << endl;
+		post_render();
+		cin.get();
+		return -1;
+	}
+	cout << "2. El directorio de historial ha sido creado correctamente" << endl;
+			
+	// 3. Initiate ray tracing
+	Render& render_manager = Render::get_instance();	
+	Image* result = render_manager.ray_tracing();
+	cout << "3. Imagen generada correctamente" << endl;
 
+	// 4. Save result image
+	if (!story_manager.save_result(result)) {
+		cerr << "Error: La imagen resultante no pudo ser guardada" << endl << endl;
+		post_render();
+		cin.get();
+		return -1;
+	}
+	cout << "4. Imagen guardada correctamente" << endl;
+			
+	post_render();
+	cin.get();
 	return 0;
 }

7530d2ff74fd8a5bc41859ce04887236180a3d46
Renzo Gambone <renzodgc12@gmail.com>
2021-06-22 17:01:03 -0300

    Release and Debug Assets
    Release and Debug Assets

--- obl2/obl2/parameters.h
+++ obl2/obl2/parameters.h
@@ -13,7 +13,7 @@ constexpr int HALF_IMAGE_HEIGHT = IMAGE_HEIGHT/2;
 constexpr int IMAGE_BITS_PER_PIXEL = 24; // 3 x 8 bit (RGB)
 
 // Ray tracing
-constexpr int MAX_DEPTH = 3;
+constexpr int MAX_DEPTH = 6;
 constexpr float FOV = 2500.f;
 const bool ANTIALIASING = false;
 const float ANTIALIASING_SHIFT = 0.25f;

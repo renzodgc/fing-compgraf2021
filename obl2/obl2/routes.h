c8fe7961e22c0a2cb6c4ad710306369fe7aa952f
Emiliano Botti <emiliano.botti@fing.edu.uy>
2021-06-23 00:34:55 -0300

    Movido todo a hightlights

--- obl2/obl2/routes.h
+++ obl2/obl2/routes.h
@@ -10,7 +10,7 @@
 // FILE ROUTES
 // ---------------------------------------------------------------------------------------
 
-const std::string CONFIG_FILE = "tests_config_full_scene_diffuse.xml";
+const std::string CONFIG_FILE = "config_3spheres.xml";
 const std::string SAVE_FOLDER = "results\\";
 
 // FILE NAMES
